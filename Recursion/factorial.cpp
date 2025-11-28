e074c425ad601ad18c21f003cdcf366654753485
Venkat Sai Kondra <venkatsaikondra67@gmail.com>
2025-11-28 14:56:02 +0530

    Fix nested repo and merge history

---
+++ Recursion/factorial.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int fac(int n){
+    if(n==1){
+        return 1;
+    }
+    return n*fac(n-1);
+}
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    cout<<fac(n);
+}
