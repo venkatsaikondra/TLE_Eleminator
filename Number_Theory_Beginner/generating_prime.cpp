f57dd6a13097afb37cd0839fdef01c978e41f7fb
Venkat Sai Kondra <venkatsaikondra67@gmail.com>
2025-12-02 14:06:50 +0530

    done with number theory

--- Number_Theory_Beginner/generating_prime.cpp
+++ Number_Theory_Beginner/generating_prime.cpp
@@ -20,6 +20,21 @@ int main(){
     }
     
 */
+ vector<int>prime(n+1,1);
+ prime[0]=0;
+ prime[1]=0;
+ for(int i=2;i*i<=n;i++){
+    if(prime[i]==1){
+        for(int j=i*i;j<n;j+=i){
+            prime[j]=0;
+        }
+    }
+ }
+ for(int i=0;i<n;i++){
+    if(prime[i]==1){
+        cout<<i<<endl;
+    }
+ }
     
     return 0;
 }
